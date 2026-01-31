991cc4aa0ac497df5c1a11d7e44cc2d88bb596fb
Jackson Henry <jhenry11094@gmail.com>
2026-01-31 15:16:03 -0600

    adding print_header and read_wav_to_buff functions

--- src/main.cpp
+++ src/main.cpp
@@ -20,31 +20,7 @@ struct WavHeader {
     uint16_t bitsPerSample;
 };
 
-
-int main() {
-    std::ifstream file("../data/tone.wav", std::ios::binary);
-    if (!file) {
-        std::cerr << "Failed to open file\n";
-        return 1;
-    }
-
-    std::vector<unsigned char> buffer(64);
-    file.read(reinterpret_cast<char*>(buffer.data()), buffer.size());
-
-    for (size_t i = 0; i < buffer.size(); ++i) {
-        std::cout << std::hex << std::setw(2) << std::setfill('0')
-                  << static_cast<int>(buffer[i]) << " ";
-        if ((i + 1) % 16 == 0) std::cout << "\n";
-    }
-    file.seekg(0);
-    char riff[4];
-    file.read(riff, 4);
-    std::cout << "ChunkID: " << std::string(riff, 4) << "\n";
-
-    WavHeader header;
-    file.seekg(0);
-    file.read(reinterpret_cast<char*>(&header), sizeof(header));
-
+void print_header(WavHeader &header) {
     std::cout << std::dec;
     std::cout << "=====Print Header======" << "\n";
     std::cout << "Chunk ID: " << std::string(header.riff,4) << "\n";
@@ -58,27 +34,57 @@ int main() {
     std::cout << "byte rate: " << header.byteRate << "\n";
     std::cout << "blockalign: " << header.blockAlign << "\n";
     std::cout << "bitPerSample: " << header.bitsPerSample << "\n";
+    std::cout << "=======================" << "\n";
+}
 
+int read_wav_to_buff(std::ifstream &file, int numSamples, std::vector<std::int16_t> &samples){
     char dataId[4];
     uint32_t dataSize;
-    
+    samples.resize(numSamples);
+
     // Read chunks until we hit "data"
-    while (true) {
+    while (file) {
         file.read(dataId, 4);
         file.read(reinterpret_cast<char*>(&dataSize), 4);
-    
+
         if (std::string(dataId, 4) == "data") {
-            break; // file pointer is now at start of samples
+            break;
         }
-    
+
         // Skip this chunk
         file.seekg(dataSize, std::ios::cur);
     }
-    int numSamples = 10;
-    std::vector<int16_t> samples(numSamples);
-    
     file.read(reinterpret_cast<char*>(samples.data()),
               numSamples * sizeof(int16_t));
+    if(!file){
+        return -1;
+    }
+
+    return 1;
+}
+
+
+
+int main() {
+    std::ifstream file("../data/tone.wav", std::ios::binary);
+    if (!file) {
+        std::cerr << "Failed to open file\n";
+        return 1;
+    }
+
+    std::vector<unsigned char> buffer(64);
+    file.read(reinterpret_cast<char*>(buffer.data()), buffer.size());
+    WavHeader header;
+    file.seekg(0);
+    file.read(reinterpret_cast<char*>(&header), sizeof(header));
+
+    print_header(header);
+     
+    int numSamples = 10;
+    std::vector<int16_t> samples(numSamples);
+
+    read_wav_to_buff(file, numSamples, samples);
+
     
     for (auto s : samples) {
         std::cout << s << " ";
